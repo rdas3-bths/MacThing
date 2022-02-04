ea5ffa9851a46464e7fdae21cce32f1c300fd640
rdas3-bths <rdas3@schools.nyc.gov>
2022-02-04 07:18:11 -0500

    Changed mac thing to 24.

--- src/RunMacThing.java
+++ src/RunMacThing.java
@@ -1,7 +1,7 @@
 public class RunMacThing {
     public static void main(String[] args) {
         System.out.println("Hello I am a Mac Thing.");
-        MacThing mt = new MacThing(20);
+        MacThing mt = new MacThing(24);
         mt.doMacThing();
     }
 }
