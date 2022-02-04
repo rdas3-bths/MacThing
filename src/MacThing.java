98b9268d3d933fc63f5afcb9b02c6eba1a5f67b8
Rupesh Das <dasru@rupeshs-mbp.78b430.nycboe.org>
2022-02-04 07:10:20 -0500

    Initial commit

---
+++ src/MacThing.java
@@ -0,0 +1,11 @@
+public class MacThing {
+    private int num;
+
+    public MacThing(int n) {
+        num = n;
+    }
+
+    public void doMacThing() {
+        System.out.println("I am Mac Thing number " + num);
+    }
+}
---
+++ src/RunMacThing.java
@@ -0,0 +1,7 @@
+public class RunMacThing {
+    public static void main(String[] args) {
+        System.out.println("Hello I am a Mac Thing.");
+        MacThing mt = new MacThing(20);
+        mt.doMacThing();
+    }
+}
